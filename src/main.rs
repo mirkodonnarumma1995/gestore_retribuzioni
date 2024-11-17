082204008a5834605a048c857db1851c1710b219
Mirko <114354815+mirkodonnarumma1995@users.noreply.github.com>
2024-11-17 23:38:19 +0100

    prima versione del programma
    il comportamento del programma al momento consente di inserire un numero intero e lo divide, ritornando un valore con 2 cifre dopo la virgola, dividendo la somma inserita in percentuali (50, 30 e 20) fornendo risultati per spese, svago e risparmi.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,31 @@
 // Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![windows_subsystem = "windows"]
 
 use std::error::Error;
 
 slint::include_modules!();
 
+const PERCENTUALE_SPESE: f64 = 0.50;
+const PERCENTUALE_SVAGO: f64 = 0.30;
+const PERCENTUALE_RISPARMI: f64 = 0.20;
+
 fn main() -> Result<(), Box<dyn Error>> {
     let ui = AppWindow::new()?;
 
-    ui.on_request_increase_value({
+    ui.on_dividi_somma({
         let ui_handle = ui.as_weak();
-        move || {
+        move |string| {
             let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
+            let num: f64 = string.trim().parse().unwrap();
+            let spese: f64 = num * PERCENTUALE_SPESE;
+            let svago: f64 = num * PERCENTUALE_SVAGO;
+            let risparmi: f64 = num * PERCENTUALE_RISPARMI;
+            let risultato: String = format!(
+                "Spese: {:.2}€\nSvago: {:.2}€\nRisparmi: {:.2}€",
+                spese, svago, risparmi
+            );
+            ui.set_risultato(risultato.into());
         }
     });
 
